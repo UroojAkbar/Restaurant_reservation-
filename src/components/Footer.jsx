7f70e22b0121c0773faf1ae7312b4e3986f3bf02
UroojAkbar <uroojakbar235@gmail.com>
2024-08-22 18:18:27 +0500

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">Ujjii</div>
+          <div className="right">
+            <p>Gullbaha, Peshawar</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By CodeByUjjii</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By UroojAkbar.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
